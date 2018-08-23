69da8261e13442ea89506a7c1bef463b5b4c185f
Samantha Sbarra <samantha.sbarra@hotmail.com>
2018-08-23 18:09:29 +0200

    ritocco 4

--- AllanMio_20180821.m
+++ AllanMio_20180821.m
@@ -4,47 +4,73 @@ format long
 
 %%introdurre la cartella di lavoro
 percorso='C:\Users\will\Dropbox\Misure\Misure-20180725';
-%%nome file sweep
-fileSweep='20180725-1s6d5-15mW-150mVpk-sweepRBM3.txt';
-%%nome file traccia temporale
-TracciaTemporale='20180725-s6d5-15mW-150mVpk-3Modes-openloop.txt';
+cd 'C:\Users\will\Dropbox\Misure\Misure-20180725'
+fileTxt = dir('*.txt');
+n_modi=0;
+for i=1:length(fileTxt)
+    k=strfind(fileTxt(i).name,'sweep');
+    if ~isempty(k)
+        n_modi=n_modi+1;
+        k=strfind(fileTxt(i).name,'RBM');
+        j=str2num(fileTxt(i).name(k+1));
+        Sweep(i)=importdata(fileTxt(i).name,';');
+    end
+end    
 
-addpath percorso;
-Sweep=importdata(fileSweep,';',8);
+for i=1:length(fileTxt)
+    k=strfind(fileTxt(i).name,'PLL');
+    z=strfind(fileTxt(i).name,'OpenLoop');
+    if ~isempty(k)||~isempty(z)
+        TracciaTemporale=importdata(fileTxt(i).name,';');
+        s=strfind(fileTxt(i).name,'s');
+        for w=1:length(s)
+            if fileTxt(i).name(s(w)+2)=='d';
+                serie=fileTxt(i).name(s(w)+1);
+                disco=fileTxt(i).name(s(w)+3);
+            end
+        end    
+        power=strfind(fileTxt(i).name,'mW');
+        power=fileTxt(i).name(power-2:power+1);
+        amplit=strfind(fileTxt(i).name,'mVpk');
+        amplit=fileTxt(i).name(amplit-3:amplit+3);
+        date=strfind(fileTxt(i).name,'2018');
+        date=fileTxt(i).name(date:date+7);
+    end   
+end    
 
-figure(1)
-xAmpli=Sweep.data(1:end/2,1);
-yAmpli=Sweep.data(1:end/2,2);
-xPhase=Sweep.data(end/2+1:end,1);
-yPhase=Sweep.data(end/2+1:end,2);
-ax1 = subplot(2,1,1);
+for i=1:n_modi
+    figure(i)
+    xAmpli=Sweep(i).data(1:end/2,1);
+    yAmpli=Sweep(i).data(1:end/2,2);
+    xPhase=Sweep(i).data(end/2+1:end,1);
+    yPhase=Sweep(i).data(end/2+1:end,2);
+    ax1 = subplot(2,1,1);
         hold on
         plot(xAmpli*1e-6,yAmpli,'linewidth',1.2 )
         ylabel(ax1,'Amplitude(dB)','fontsize',18)
         xlabel(ax1,'Frequency (MHz)','fontsize',18);
         box on
-ax2 = subplot(2,1,2);
+    ax2 = subplot(2,1,2);
         hold on
         plot(xPhase*1e-6,yPhase,'linewidth',1.2 )  
         ylabel(ax2,'Phase(deg)','fontsize',18)
         xlabel(ax2,'Frequency (MHz)','fontsize',18);
         box on
-[xP,yP]=ginput(3)
-slope=(xP(3)-xP(2))/(yP(3)-yP(2));
-f0=xP(1);
-close
-
-%%scrittura file con punti scelti , slope, f0. Inserire Percorso di
-%%scrittura
-fileName=percorso+"\s6d5-OpticalModulation-RBM3-15mW-150mVpk.DAT";
-f = fopen(fileName,'w')
-fprintf(f,'%5s %f %f\n','P(1)',xP(1),yP(1));
-fprintf(f,'%5s %f %f\n','P(2)',xP(2),yP(2));
-fprintf(f,'%5s %f %f\n\n','P(3)',xP(3),yP(3));
-fprintf(f,'%2s %f\n','f0',f0);
-fprintf(f,'%5s %f\n','slope',slope);
-fclose(f);
-
+    [xP,yP]=ginput(3)
+    slope(i)=(xP(3)-xP(2))/(yP(3)-yP(2));
+    f0(i)=xP(1);
+    close
+    %%scrittura file con punti scelti , slope, f0. Inserire Percorso di
+    %%scrittura
+    fileName=percorso+"\s6d5-OpticalModulation-RBM3-15mW-150mVpk.DAT";
+    f = fopen(fileName,'w')
+    fprintf(f,'%5s %f %f\n','P(1)',xP(1),yP(1));
+    fprintf(f,'%5s %f %f\n','P(2)',xP(2),yP(2));
+    fprintf(f,'%5s %f %f\n\n','P(3)',xP(3),yP(3));
+    fprintf(f,'%2s %f\n','f0',f0(i));
+    fprintf(f,'%5s %f\n','slope',slope(i));
+    fclose(f);
+end
 % importare file della traccia temporale della fase
 PhaseTimeTrace=importdata(TracciaTemporale,';');
 Pos_fin=find(PhaseTimeTrace.data(:,1)==0); %%identifica la posizione nel vettore tempo di t=0 che corrisponde all'ultimo acquisito per ogni modo
